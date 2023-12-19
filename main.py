cfc2b1fd1c760f226d73640357080c6f73e8e6be
Marius Verdier <marius.verdier@student-cs.fr>
2023-12-19 18:01:12 +0100

    Add chaotic behavior

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import time
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 from brute_force import BruteForceSimulation
 from barnes_hut import BarnesHutSimulation
+
+def Bodies(t,y):
+    f = np.zeros(12)
+    f[0] = y[6]
+    f[1] = y[7]
+    f[2] = y[8]
+    f[3] = y[9]
+    f[4] = y[10]
+    f[5] = y[11]
+    
+    f[6] = -(y[0]-y[2])/(((y[0]-y[2])**2+(y[1]-y[3])**2)**(3/2)) \
+           -(y[0]-y[4])/(((y[0]-y[4])**2+(y[1]-y[5])**2)**(3/2))
+           
+    f[7] = -(y[1]-y[3])/(((y[0]-y[2])**2+(y[1]-y[3])**2)**(3/2)) \
+           -(y[1]-y[5])/(((y[0]-y[4])**2+(y[1]-y[5])**2)**(3/2))
+                     
+    f[8] = -(y[2]-y[0])/(((y[2]-y[0])**2+(y[3]-y[1])**2)**(3/2)) \
+           -(y[2]-y[4])/(((y[2]-y[4])**2+(y[3]-y[5])**2)**(3/2))
+           
+    f[9] = -(y[3]-y[1])/(((y[2]-y[0])**2+(y[3]-y[1])**2)**(3/2)) \
+           -(y[3]-y[5])/(((y[2]-y[4])**2+(y[3]-y[5])**2)**(3/2))
+                     
+    f[10]= -(y[4]-y[0])/(((y[4]-y[0])**2+(y[5]-y[1])**2)**(3/2)) \
+           -(y[4]-y[2])/(((y[4]-y[2])**2+(y[5]-y[3])**2)**(3/2))
+          
+    f[11]= -(y[5]-y[1])/(((y[4]-y[0])**2+(y[5]-y[1])**2)**(3/2)) \
+           -(y[5]-y[3])/(((y[4]-y[2])**2+(y[5]-y[3])**2)**(3/2))    
+           
+    return(f)
 
 def run_simulations():
     # Common parameters
@@ -32,4 +64,45 @@ def run_simulations():
     print(f"Barnes-Hut: {bh_duration:.2f} seconds")
 
 if __name__ == "__main__":
+
+    """
+    Comparison of the two methods
+    """
     run_simulations()
+
+    """
+    Highligting chaotic behaviour
+    """
+    y0 = [
+        -0.500004,#x1
+        0,#y1
+        0.5 ,#x2
+        0,#y2
+        0,#x3
+        0,#y3
+        0,#vx1
+        1,#vy1
+        0,#vx2
+        -1,#vy2
+        0,#vx3
+        0#vy3
+    ] 
+
+    N = 10000
+    T = 0.001 #N*T=10
+
+    t = np.linspace(0,N*T,N)
+    solution = solve_ivp(Bodies,[0,800],y0,t_eval=t,rtol=1e-13)
+
+    # Évolution des positions
+    plt.plot(solution.y[0],solution.y[1],'-g') #Corps 1
+    plt.plot(solution.y[2],solution.y[3],'-r') #Corps 2
+    plt.plot(solution.y[4],solution.y[5],'-b') #Corps 3
+    plt.ylabel("Position(y)")
+    plt.xlabel("Position(x)")
+    plt.show()
+
+    plt.plot(t,solution.y[6])
+    plt.ylabel("Position (x)")
+    plt.xlabel("Temps")
+    plt.show()
